ec833b633f5c6b13114c83366a18bdba0223c8e7
Eduardo Rolim <ens.rolim@gmail.com>
2019-04-25 21:54:45 -0300

    Corrigida atribuição do blueprint

--- sd_app/admin/views.py
+++ sd_app/admin/views.py
@@ -4,9 +4,9 @@
 from flask import render_template
 from flask_login import login_required
 
-from . import main
+from . import admin
 
-@main.route('/')
+@admin.route('/')
 @login_required
 def get_admin():
     return render_template('admin.html')
