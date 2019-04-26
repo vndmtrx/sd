47ed6c93022f86b79f841edfe823fce1d2d18f4c
Eduardo Rolim <ens.rolim@gmail.com>
2019-04-25 22:15:01 -0300

    Alterações na inicialização do app.

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# http://pythonclub.com.br/what-the-flask-pt-1-introducao-ao-desenvolvimento-web-com-python.html
 
-from sd_app import app
+from sd_app import create_app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app(config='config.py')
+    app.run(debug=True, use_reloader=True)
--- sd_app/__init__.py
+++ sd_app/__init__.py
@@ -7,9 +7,15 @@ from flask_login import LoginManager
 from .main import main
 from .admin import admin
 
-app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-app.register_blueprint(main, url_prefix='/')
-app.register_blueprint(admin, url_prefix='/admin')
+def create_app(config=None):
+    app = Flask(__name__)
+    if config:
+        app.config.from_pyfile(config)
+    
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    
+    app.register_blueprint(main, url_prefix='/')
+    app.register_blueprint(admin, url_prefix='/admin')
+    
+    return app
