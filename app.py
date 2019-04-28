d29e20289e3a3ac94678d7fee1e2ddefcab64bd3
Eduardo Rolim <ens.rolim@gmail.com>
2019-04-28 13:01:53 -0300

    Adicionado preenchimento do contexto para uso com o comando `flask shell`.

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ import os
 from sd_app import create_app
 
 cfg = os.path.abspath('config.cfg')
+
 app = create_app(config=cfg)
+
+@app.shell_context_processor
+def cria_contexto_shell():
+    from sd_app.compartilhado import sqlalchemy as banco
+    from sd_app.usuarios.models import Usuario
+    
+    return {
+        'banco': banco,
+        'Usuario': Usuario
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
