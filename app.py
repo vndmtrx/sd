245c4569b8bc3bca947a8d972b7e3c271cb21446
Eduardo Rolim <ens.rolim@gmail.com>
2019-04-28 01:28:59 -0300

    Renomeados arquivos de configuração e carregamento pelo comando Flask.

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 # http://pythonclub.com.br/what-the-flask-pt-1-introducao-ao-desenvolvimento-web-com-python.html
 # http://flask.pocoo.org/docs/1.0/patterns/
+import os
 
 from sd_app import create_app
 
+cfg = os.path.abspath('config.cfg')
+app = create_app(config=cfg)
+
 if __name__ == '__main__':
-    app = create_app(config='config.py')
     app.run(debug=True, use_reloader=True)
