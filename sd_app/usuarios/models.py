380f144bb64f078823172c005269969e110286ee
Eduardo Rolim <ens.rolim@gmail.com>
2019-04-28 13:51:02 -0300

    Alteração do model Usuario.

--- sd_app/usuarios/models.py
+++ sd_app/usuarios/models.py
@@ -6,9 +6,9 @@ from sd_app.compartilhado import sqlalchemy as banco
 class Usuario(banco.Model):
     __tablename__ = 'usuarios'
     id = banco.Column(banco.Integer, primary_key=True)
-    login = banco.Column(banco.String(20), unique=True, nullable=False)
+    login = banco.Column(banco.String(20), unique=True, index=True, nullable=False)
+    email = banco.Column(banco.String(50), unique=True, index=True, nullable=False)
     nome = banco.Column(banco.String(80), nullable=False)
-    email = banco.Column(banco.String(50), unique=True, nullable=False)
     
     def __repr__(self):
         return '<Usuario {0}>'.format(self.usuario)
