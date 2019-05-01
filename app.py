f3cd979a22bbb53322e3b25c8d3fa51707b8ffbc
Eduardo Rolim <ens.rolim@gmail.com>
2019-05-01 20:03:33 -0300

    Adicionada variável de ambiente necessária para mostrar modo de debug

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 # http://pythonclub.com.br/what-the-flask-pt-1-introducao-ao-desenvolvimento-web-com-python.html
 # http://flask.pocoo.org/docs/1.0/patterns/
+
+## export FLASK_ENV=development
+
 import os
 
 from sd_app import create_app
